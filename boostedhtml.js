0b7595d4560d43d1221e1f647d2f89d3e8bec92a
Andrea Licheri <126976233+andrealicheri@users.noreply.github.com>
2023-09-03 16:44:25 +0200

    Fixed scoped attr
    The scoped attribute would generate horrible, broken css. It's now fixed. Now WIP fixing at-rules too

--- boostedhtml.js
+++ boostedhtml.js
@@ -1,4 +1,5 @@
 // boosted-html main framework
+
 function normalize() {
     const linkElement = document.createElement("style");
     linkElement.innerHTML = `progress,sub,sup{vertical-align:baseline}*,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;-moz-tab-size:4;tab-size:4}body{margin:0}hr{height:0;color:inherit}abbr[title]{text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative}sub{bottom:-.25em}sup{top:-.5em}table{text-indent:0;border-color:inherit}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}::-moz-focus-inner{border-style:none;padding:0}:-moz-focusring{outline:ButtonText dotted 1px}:-moz-ui-invalid{box-shadow:none}legend{padding:0}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}`
@@ -60,7 +61,7 @@ function componentTag() {
     const comps = document.querySelectorAll('[component]')
     comps.forEach(element => {
         let outerHTML = element.outerHTML;
-        let innerHTML = element.innerHTML; 
+        let innerHTML = element.innerHTML;
         let tag = outerHTML.split("<")[1].split(" ")[0]
         let toUse = document.createElement("read")
         toUse.innerHTML = innerHTML
@@ -141,19 +142,22 @@ function nestedTemplate() {
     const scopedElements = document.querySelectorAll('[scoped]');
     scopedElements.forEach(element => {
         const randomId = generateRandomString(32);
-        element.setAttribute("class", randomId)
-        element.setAttribute("style", "all:initial")
+        element.setAttribute("class", randomId);
+        element.setAttribute("style", "all: initial");
         const styleTags = element.querySelectorAll('style');
         styleTags.forEach(styleTag => {
             const cssText = styleTag.textContent.trim();
-            const modifiedCssText = cssText.replace(/([^{]+)\{/g, (_, selectors) => {
-                const modifiedSelectors = selectors.split(',').map(selector => `.${randomId} ${selector.trim()}`).join(', ');
+            const modifiedCssText = cssText.replace(/([^{}]+)\{/g, (match, selectors) => {
+                const modifiedSelectors = selectors
+                    .split(',')
+                    .map(selector => `.${randomId} ${selector.trim()}`)
+                    .join(', ');
                 return `${modifiedSelectors} {`;
             });
             styleTag.textContent = modifiedCssText;
         });
     });
-};
+}
 
 async function main() {
     idCheck()
@@ -223,11 +227,11 @@ window.addEventListener('popstate', () => updateBodyContent(window.location.path
 
 const elementsWithRouteAttribute = document.querySelectorAll('[route]');
 elementsWithRouteAttribute.forEach((element) => {
-  const anchorElements = element.querySelectorAll('a');
-  anchorElements.forEach((anchor) => {
-    anchor.addEventListener('click', (event) => {
-        event.preventDefault();
-        navigateTo(anchor.getAttribute('href'));
+    const anchorElements = element.querySelectorAll('a');
+    anchorElements.forEach((anchor) => {
+        anchor.addEventListener('click', (event) => {
+            event.preventDefault();
+            navigateTo(anchor.getAttribute('href'));
+        });
     });
-  });
 });
