911d3e3b3a6e4a08f2a787805e43e0227537b529
andrealicheri <andrealicheripakunza@gmail.com>
2023-09-16 14:57:39 +0200

    Added templates

---
+++ .templates/sw.js
@@ -0,0 +1,19 @@
+// Caching
+const cacheName = 'MyFancyCacheName_v1';
+self.addEventListener('fetch', (event) => {
+  if (event.request.destination === 'image') {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+  } else {
+    return;
+  }
+});
