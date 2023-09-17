17007f292ea12772ef27c086ff66bb1d3bc3639a
andrealicheri <andrealicheripakunza@gmail.com>
2023-09-17 11:05:45 +0200

    Added Blaze executable

--- cli/build.py
+++ cli/build.py
@@ -1,10 +1,21 @@
 import PyInstaller.__main__
+import os
+import shutil
 
 def bundle_scripts():
+    # Specify PyInstaller command line arguments
     args = [
-        '--onefile',        
-        'blaze.py' 
+        '--onefile',
+        'blaze.py'
     ]
     PyInstaller.__main__.run(args)
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    if os.path.exists('blaze.spec'):
+        os.remove('blaze.spec')
+    os.rename("dist/blaze.exe", "blaze.exe")
+    if os.path.exists('dist'):
+        shutil.rmtree('dist')
+
 if __name__ == "__main__":
     bundle_scripts()
