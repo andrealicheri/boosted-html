55946770752a8e55b378309150a11a9b1c5ab976
andrealicheri <andrealicheripakunza@gmail.com>
2023-09-17 11:19:29 +0200

    Updated build script

--- cli/blaze.py
+++ cli/blaze.py
@@ -5,6 +5,7 @@ import os
 import http.server
 import socketserver
 
+
 def download(url, name):
     r = requests.get(url, allow_redirects=True)
     open(name, "wb").write(r.content)
--- cli/build.py
+++ cli/build.py
@@ -3,19 +3,16 @@ import os
 import shutil
 
 def bundle_scripts():
-    # Specify PyInstaller command line arguments
     args = [
         '--onefile',
         'blaze.py'
     ]
     PyInstaller.__main__.run(args)
-    if os.path.exists('build'):
-        shutil.rmtree('build')
-    if os.path.exists('blaze.spec'):
-        os.remove('blaze.spec')
+    os.remove("blaze.exe")
+    shutil.rmtree('build')
+    os.remove('blaze.spec')
     os.rename("dist/blaze.exe", "blaze.exe")
-    if os.path.exists('dist'):
-        shutil.rmtree('dist')
+    shutil.rmtree('dist')
 
 if __name__ == "__main__":
     bundle_scripts()
